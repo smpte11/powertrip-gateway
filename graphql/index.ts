fd569d02281c630ab106d6e4e2bc7ca42779abf6
Felix Brousseau-Tremblay <felix.brousseau-tremblay.1@live.com>
2020-05-30 17:49:05 +0200

    Make augmenting context more robust

--- graphql/index.ts
+++ graphql/index.ts
@@ -10,6 +10,17 @@ import TravelResolver from "./travel/resolvers";
 import WeatherResolver from "./weather/resolvers";
 
 import buildConfig, { Configurable } from "./config";
+import { admin } from "firebase-admin/lib/auth";
+
+type Request = {
+  headers: {
+    [key: string]: string;
+  };
+};
+
+type ContextType = {
+  user: admin.auth.UserInfo;
+};
 
 Container.set({ id: "config", factory: buildConfig });
 
@@ -19,33 +30,23 @@ const schema = buildSchemaSync({
   emitSchemaFile: true,
 });
 
-type Request = {
-  headers: {
-    [key: string]: string;
-  };
-};
-
 const server = new ApolloServer({
   //@ts-ignore
   schema,
   playground: true,
   mocks: (Container.get("config") as Configurable).mocks,
   mockEntireSchema: (Container.get("config") as Configurable).mockEntireSchema,
-  context: async function ({ request }) {
-    let user = null;
+  context: async function ({ request }: { request: Request }) {
     const config = Container.get<Configurable>("config");
-
-    // TODO check empty case
     const token = request.headers.authorization.split(" ").slice(-1)[0];
 
     try {
-      // TODO check refresh token
-      user = await config.authService.verifyIdToken(token);
+      const decodedToken = await config.authService.verifyIdToken(token);
+      const user = await config.authService.getUser(decodedToken.uid);
+      return { user };
     } catch (error) {
-      console.log(error);
+      return {};
     }
-
-    return { user };
   },
 });
 
